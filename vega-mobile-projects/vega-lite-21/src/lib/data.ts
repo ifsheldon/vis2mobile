f375181ad396a6c53b7bcc5141c2a7e92c13065d
ifsheldon <feng.liang@kaust.edu.sa>
2026-01-13 20:58:51 +0800

    finished vega-lite-21

--- vega-mobile-projects/vega-lite-21/src/lib/data.ts
+++ vega-mobile-projects/vega-lite-21/src/lib/data.ts
@@ -1,57 +1,15 @@
-export interface PlanData {
-  sector: string;
-  republican: number;
-  passed: number;
-  democratic: number;
+import carsData from "./cars.json";
+
+export interface Car {
+  Name: string;
+  Miles_per_Gallon: number | null;
+  Cylinders: number;
+  Displacement: number;
+  Horsepower: number | null;
+  Weight_in_lbs: number;
+  Acceleration: number;
+  Year: string;
+  Origin: string;
 }
 
-export const data: PlanData[] = [
-  {
-    "sector": "Small-business aid",
-    "republican": 200,
-    "passed": 1010,
-    "democratic": 0
-  },
-  {
-    "sector": "Other measures",
-    "republican": 81,
-    "passed": 627,
-    "democratic": 302
-  },
-  {
-    "sector": "Business tax breaks",
-    "republican": 203,
-    "passed": 346,
-    "democratic": 36
-  },
-  {
-    "sector": "Stimulus checks",
-    "republican": 300,
-    "passed": 293,
-    "democratic": 436
-  },
-  {
-    "sector": "Health care",
-    "republican": 111,
-    "passed": 277,
-    "democratic": 382
-  },
-  {
-    "sector": "Unemployment benefits",
-    "republican": 110,
-    "passed": 274,
-    "democratic": 437
-  },
-  {
-    "sector": "State and local aid",
-    "republican": 105,
-    "passed": 256,
-    "democratic": 1118
-  },
-  {
-    "sector": "Safety net and other tax cuts",
-    "republican": 18,
-    "passed": 83,
-    "democratic": 736
-  }
-];
+export const cars: Car[] = carsData as Car[];
