ebdea84ea0f1982e9bfbe5460169f166a6a86f47
ifsheldon <feng.liang@kaust.edu.sa>
2026-01-14 01:41:31 +0800

    Add vega-03 to gallery

---
+++ gallery/app/preview/vega/vega-03/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Visualization } from "@/components/vega/vega-03/Visualization";
+
+export default function Vega03PreviewPage() {
+	return (
+		<div className="h-screen">
+			<Visualization />
+		</div>
+	);
+}
