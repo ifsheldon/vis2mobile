d0b3b15d60ee219dfca1cb9eeb5216834b2049cc
ifsheldon <feng.liang@kaust.edu.sa>
2026-01-14 02:45:38 +0800

    Add vega-altair-08 to gallery

---
+++ gallery/components/vega/vega-altair-08/Visualization.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function Visualization() {
+	return (
+		<div className="w-full h-full flex items-center justify-center text-zinc-500">
+			TODO: Visualization of Mobile Version here
+		</div>
+	);
+}
