2175b6a9b30fa556acdabd84dbf29e634bdd0f52
ifsheldon <feng.liang@kaust.edu.sa>
2026-01-14 10:40:00 +0800

    Add vega-altair-14 to gallery

---
+++ gallery/app/preview/vega/vega-altair-14/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Visualization } from "@/components/vega/vega-altair-14/Visualization";
+
+export default function VegaAltair14PreviewPage() {
+	return (
+		<div className="h-screen">
+			<Visualization />
+		</div>
+	);
+}
