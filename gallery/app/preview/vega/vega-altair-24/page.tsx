16f6edc9609d5ab7923ac50784e69b88bad70146
ifsheldon <feng.liang@kaust.edu.sa>
2026-01-14 12:00:02 +0800

    Add vega-altair-24 to gallery

---
+++ gallery/app/preview/vega/vega-altair-24/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Visualization } from "@/components/vega/vega-altair-24/Visualization";
+
+export default function VegaAltair24PreviewPage() {
+	return (
+		<div className="h-screen">
+			<Visualization />
+		</div>
+	);
+}
