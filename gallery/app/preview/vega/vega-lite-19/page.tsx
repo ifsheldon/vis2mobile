5b567d8f410e5e4039dac3b2dca5cd0871ce061f
ifsheldon <feng.liang@kaust.edu.sa>
2026-01-14 15:42:19 +0800

    Add vega-lite-19 to gallery

---
+++ gallery/app/preview/vega/vega-lite-19/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Visualization } from "@/components/vega/vega-lite-19/Visualization";
+
+export default function VegaLite19PreviewPage() {
+	return (
+		<div className="h-screen">
+			<Visualization />
+		</div>
+	);
+}
